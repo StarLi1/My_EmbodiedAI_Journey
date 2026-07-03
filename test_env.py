5fa13bd0fdf12e4f52685beb310f067811734b58
StarLi1 <1727838956@qq.com>
2026-07-03 18:57:30 +0800

    feat: 真正的第一次本地存档

---
+++ test_env.py
@@ -0,0 +1,6 @@
+import sys
+user_name = "准研究生"
+direction = "具身智能（Embodied AI）"
+print(f"🎉 欢迎来到控制工程科研世界，{user_name}！")
+print(f"🚀 你的研究方向已锁定：{direction}")
+print(f"📦 当前运行环境的 Python 版本是: {sys.version}")
